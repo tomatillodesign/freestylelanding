00d4103cf015f0cd2735e7c6576dcb0ded9ea46e
tomatillodesign <chris@tomatillodesign.com>
2019-02-01 12:30:50 -0500

    before changing react app code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. CLB TESTING!
+            Edit <code>src/App.js</code> and save to reload.<br/>One final test before working on Freestyle...
           </p>
           <a
             className="App-link"
