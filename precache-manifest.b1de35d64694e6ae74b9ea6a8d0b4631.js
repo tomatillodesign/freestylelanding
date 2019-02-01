68a8043d74a9c26c6bffeaa38cf6b6805de3bbc1
tomatillodesign <chris@tomatillodesign.com>
2019-02-01 12:19:46 -0500

    Updates

---
+++ precache-manifest.b1de35d64694e6ae74b9ea6a8d0b4631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab063c5bf40720480a61",
+    "url": "/freestylelanding/static/css/main.d5b9cc2e.chunk.css"
+  },
+  {
+    "revision": "ab063c5bf40720480a61",
+    "url": "/freestylelanding/static/js/main.ab063c5b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/freestylelanding/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "17c8345ceb603ec37c10",
+    "url": "/freestylelanding/static/js/runtime~main.17c8345c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/freestylelanding/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9c82502ecc04f60bea6d151313168cd0",
+    "url": "/freestylelanding/index.html"
+  }
+];
